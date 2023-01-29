39eca582d1bf1b076d130c33953cfc76a9438309
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 22:12:45 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int crystalCost = 13;
+Console.WriteLine("Здравствуй, путник, сколько золотых у тебя в карманах?");
+int playerGold = int.Parse(Console.ReadLine());
+Console.WriteLine("И сколько кристаллов вы хотите на них купить? В моем магазине они дешевые - всего по 13 монет.");
+int quantityPurchased = int.Parse(Console.ReadLine());
+int remainingGold = playerGold - quantityPurchased * crystalCost;
+Console.WriteLine($"У вас в инвентаре {remainingGold} золота и {quantityPurchased} кристаллов.");
